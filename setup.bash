b22cc081ccd43b17891571a22a461d1dd89cac5a
Robert F Cooper <robertfcoop@gmail.com>
2018-10-08 15:08:56 -0400

    Created a bash script for setting up a linux/mac virtual environment.

---
+++ setup.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+THISPATH=$(dirname "$BASH_SOURCE")
+
+THISPATH="$(cd "$THISPATH" && pwd)"
+
+echo Creating virtual environment in: "$THISPATH"
+
+# Install virtualenv, if it doesnt' exist.
+python3 -m pip install virtualenv
+# Install the virtual environment
+python3 -m virtualenv "$THISPATH/venv"
+
+# Activate our virtualenv, then install wxPython 4 and numpy.
+source "$THISPATH/venv/bin/activate"
+python3 -m pip install wxPython && python3 -m pip install numpy
+deactivate
