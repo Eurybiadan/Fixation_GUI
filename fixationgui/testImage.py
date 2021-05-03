d9bd6cfd003d6ef61257944819c2e495ce5f0c8c
jengriesh <jengriesh@gmail.com>
2021-05-03 11:05:04 -0500

    Affine Transformation
    Image transformation using 3 points complete on testImage. Next need to integrate it back into main gui

--- fixationgui/testImage.py
+++ fixationgui/testImage.py
@@ -1,7 +1,12 @@
 import math
 import os
 import wx
+from matplotlib import cm
+
 from ViewPane import ViewPane
+import numpy
+import cv2
+from PIL import Image
 
 
 class wxImageFrame(wx.Frame):
@@ -65,8 +70,11 @@ class wxImageFrame(wx.Frame):
         self.imagespace.SetBackgroundColour('black')
         self.imagepane = ViewPane(self.imagespace, size=(513, 513))
 
+    def call_set_bkrgd(self, bkgrdim):
+        self.imagepane.set_bkgrd(bkgrdim)
+
     def initSidePane(self, parent):
-        self.sidepane = SidePane(parent, id=wx.ID_ANY)
+        self.sidepane = SidePane(parent, self.imagepane, id=wx.ID_ANY)
 
     def init_menubar(self):
 
@@ -77,43 +85,11 @@ class wxImageFrame(wx.Frame):
         menubar.Append(fileMenu, 'File')
 
         # Open a background image
-        fileMenu.Append(wx.ID_OPEN, 'Open Background Image...\tCtrl+B')
-        self.Bind(wx.EVT_MENU, self.on_open_background_image, id=wx.ID_OPEN)
+        # fileMenu.Append(wx.ID_OPEN, 'Open Background Image...\tCtrl+B')
+        # self.Bind(wx.EVT_MENU, self.on_open_background_image, id=wx.ID_OPEN)
 
         # Compounds the Menu Bar
         self.SetMenuBar(menubar)
-
-    def on_open_background_image(self, evt=None):
-        dialog = wx.FileDialog(self, 'Select background image:', self.header_dir, self.filename,
-                               'Image files (*.jpg,*.jpeg,*.bmp,*.png,*.tif,*.tiff)| *.jpg;*.jpeg;*.bmp;*.png;*.tif;*.tiff|' +
-                               'JP(E)G images (*.jpg,*.jpeg)|*.jpg;*.jpeg|BMP images (*.bmp)|*.bmp' +
-                               '|PNG images (*.png)|*.png|TIF(F) images (*.tif,*.tiff)|*.tif;*.tiff', wx.FD_OPEN)
-        if dialog.ShowModal() == wx.ID_OK:
-            self.header_dir = dialog.GetDirectory()
-            self.filename = dialog.GetFilename()
-            dialog.Destroy()
-            impath = self.header_dir + os.sep + self.filename
-
-            bkgrdim = wx.Bitmap(1, 1)
-            bkgrdim.LoadFile(impath, wx.BITMAP_TYPE_ANY)
-            self.imagepane.set_bkgrd(bkgrdim)
-
-            dlg = wx.TextEntryDialog(self, 'Image Scale X Dimension:', 'Image Scale')
-            if dlg.ShowModal() == wx.ID_OK:
-                self.xdim = dlg.GetValue()
-            dlg.Destroy()
-            print('x dimension is: ', int(self.xdim))
-
-            dlg = wx.TextEntryDialog(self, 'Image Scale Y Dimension:', 'Image Scale')
-            if dlg.ShowModal() == wx.ID_OK:
-                self.ydim = dlg.GetValue()
-            dlg.Destroy()
-            print('y dimension is: ', int(self.ydim))
-
-            # make a string out of the resolution dimensions
-            resolution = str(self.xdim) + ' x ' + str(self.ydim)
-            # reset the res label from No Image to the resolution that was entered
-            self.sidepane.res.SetLabel(resolution)
 
     def on_mouse_motion(self, event):
         pos = event.GetPosition()
@@ -205,7 +181,7 @@ class wxImageFrame(wx.Frame):
 
 
 class SidePane(wx.Panel):
-    def __init__(self, parent, id=-1, pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.SIMPLE_BORDER, name=''):
+    def __init__(self, parent, imagepane, id=-1, pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.SIMPLE_BORDER, name=''):
         super(SidePane, self).__init__(parent, id, pos, size, style, name)
 
         self.SetBackgroundColour('black')
@@ -215,20 +191,20 @@ class SidePane(wx.Panel):
         self.quicklabel.SetForegroundColour('white')
         self.quicklabel.SetFont(labelFont)
 
-        self.reslabel = wx.StaticText(self, wx.ID_ANY, 'Image Resolution: ', style=wx.ALIGN_CENTER)
-        self.reslabel.SetForegroundColour('white')
-        self.reslabel.SetFont(labelFont)
+        # self.reslabel = wx.StaticText(self, wx.ID_ANY, 'Image Resolution: ', style=wx.ALIGN_CENTER)
+        # self.reslabel.SetForegroundColour('white')
+        # self.reslabel.SetFont(labelFont)
 
-        self.res = wx.StaticText(self, wx.ID_ANY, 'No Image', style=wx.ALIGN_CENTER)
-        self.res.SetForegroundColour('white')
-        self.res.SetFont(labelFont)
+        # self.res = wx.StaticText(self, wx.ID_ANY, 'No Image', style=wx.ALIGN_CENTER)
+        # self.res.SetForegroundColour('white')
+        # self.res.SetFont(labelFont)
 
-        self._zoom = Buttons(self, parent)
+        self._zoom = Buttons(self, parent, imagepane, self)
 
         sizer = wx.GridBagSizer()
         sizer.Add(self.quicklabel, (1, 0), (1, 4), wx.ALIGN_CENTER)
-        sizer.Add(self.reslabel, (4, 0), (1, 4), wx.ALIGN_CENTER)
-        sizer.Add(self.res, (5, 0), (1, 4), wx.ALIGN_CENTER)
+        # sizer.Add(self.reslabel, (4, 0), (1, 4), wx.ALIGN_CENTER)
+        # sizer.Add(self.res, (5, 0), (1, 4), wx.ALIGN_CENTER)
 
         sizer.Add(self._zoom, (7, 0), (1, 4), wx.ALIGN_CENTER | wx.EXPAND)
 
@@ -243,58 +219,216 @@ class Buttons(wx.Panel):
     classdocs
     '''
 
-    def __init__(self, parent, rootparent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=(-1, -1), style=wx.SIMPLE_BORDER,
+    def __init__(self, parent, imagepane, sidepane, rootparent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=(-1, -1), style=wx.SIMPLE_BORDER,
                  name='Quick Locations Panel', port=None):
         super(Buttons, self).__init__(parent, id, pos, size, style, name)
 
+        self.header_dir = ""
+        self.filename = ""
         self.buttonList = []
         self._rootparent = rootparent
         self.SetBackgroundColour('black')
+        self.imagepaneref = imagepane
+        self.sidepane = sidepane
+        self.tracker = 0
+        self.ImageCoords = numpy.empty((0, 2), float)
+        self.LiveCoords = numpy.empty((0, 2), float)
+
 
         self.__deg_symbol = u'\N{DEGREE SIGN}'
 
         buttonalignment = wx.ALIGN_CENTER
 
-        # Zoom
-        self.zoom = wx.Button(self, label='Zoom', size=(-1, 30))
-        self.zoom.SetBackgroundColour('medium gray')
-        self.zoom.SetForegroundColour('white')
-        self.buttonList.append(self.zoom)
+        # AddImage
+        self.AddImage = wx.Button(self, label='Load Background Image', size=(-1, 30))
+        self.AddImage.SetBackgroundColour('medium gray')
+        self.AddImage.SetForegroundColour('white')
+        self.buttonList.append(self.AddImage)
 
-        # Pan
-        self.pan = wx.Button(self, label='Pan', size=(-1, 30))
-        self.pan.SetBackgroundColour('medium gray')
-        self.pan.SetForegroundColour('white')
-        self.buttonList.append(self.pan)
+        # Calibration & Select button
+        self.Cali = wx.Button(self, label='  Start Image Calibration  ', size=(-1, 30))
+        self.Cali.SetBackgroundColour('medium gray')
+        self.Cali.SetForegroundColour('white')
+        self.buttonList.append(self.Cali)
 
-        # Rotate
-        self.rotate = wx.Button(self, label='Rotate', size=(-1, 30))
-        self.rotate.SetBackgroundColour('medium gray')
-        self.rotate.SetForegroundColour('white')
-        self.buttonList.append(self.rotate)
+        # # Zoom
+        # self.zoom = wx.Button(self, label='Zoom', size=(-1, 30))
+        # self.zoom.SetBackgroundColour('medium gray')
+        # self.zoom.SetForegroundColour('white')
+        # self.buttonList.append(self.zoom)
+        #
+        # # Pan
+        # self.pan = wx.Button(self, label='Pan', size=(-1, 30))
+        # self.pan.SetBackgroundColour('medium gray')
+        # self.pan.SetForegroundColour('white')
+        # self.buttonList.append(self.pan)
+        #
+        # # Rotate
+        # self.rotate = wx.Button(self, label='Rotate', size=(-1, 30))
+        # self.rotate.SetBackgroundColour('medium gray')
+        # self.rotate.SetForegroundColour('white')
+        # self.buttonList.append(self.rotate)
 
         # Bind each button to a listener
         for button in self.buttonList:
             button.Bind(wx.EVT_BUTTON, self.OnButton)
 
         sizer = wx.GridBagSizer()
-        sizer.Add(self.zoom, (0, 0), (1, 0), buttonalignment)
-        sizer.Add(self.pan, (1, 0), (1, 0), buttonalignment)
-        sizer.Add(self.rotate, (2, 0), (1, 0), buttonalignment)
+        sizer.Add(self.AddImage, (0, 0), (1, 0), buttonalignment)
+        sizer.Add(self.Cali, (1, 0), (1, 0), buttonalignment)
+        # sizer.Add(self.zoom, (2, 0), (1, 0), buttonalignment)
+        # sizer.Add(self.pan, (3, 0), (1, 0), buttonalignment)
+        # sizer.Add(self.rotate, (4, 0), (1, 0), buttonalignment)
 
         box = wx.BoxSizer(wx.VERTICAL)  # To make sure it stays centered in the area it is given
         box.Add(sizer, 0, wx.ALIGN_CENTER)
 
         self.SetSizerAndFit(box)
 
+
     def OnButton(self, evt):
+
         pressed = evt.GetEventObject()
-        if pressed == self.zoom:
-            print("Hello")
-        if pressed == self.pan:
-            print("How are you?")
-        if pressed == self.rotate:
-            print("Bomb diggity")
+        if pressed == self.AddImage:
+            self.on_open_background_image()
+        if pressed == self.Cali:
+            if self.tracker == 0:
+                self.Cali.SetLabel('Select 1st Point on Image')
+                self.tracker = self.tracker + 1
+                # make sure the matrices are empty
+                self.ImageCoords = numpy.empty((0, 2), float)
+                self.LiveCoords = numpy.empty((0, 2), float)
+
+            elif self.tracker == 1:
+                # coordinates from 1st spot on image
+                coordinates = self.imagepaneref.imagepane._fixLoc
+                self.ptim1 = numpy.float32(coordinates)
+
+                # change button label for next point
+                self.Cali.SetLabel('Select Corresponding Point')
+                self.tracker = self.tracker + 1
+
+            elif self.tracker == 2:
+                # coordinates from 1st corresponding spot on live
+                coordinates = self.imagepaneref.imagepane._fixLoc
+                self.ptli1 = numpy.float32(coordinates)
+
+                # change button label for next point
+                self.Cali.SetLabel('Select 2nd Point on Image')
+                self.tracker = self.tracker + 1
+
+            elif self.tracker == 3:
+                # coordinates from 2nd spot on image
+                coordinates = self.imagepaneref.imagepane._fixLoc
+                self.ptim2 = numpy.float32(coordinates)
+
+                # change button label for next point
+                self.Cali.SetLabel('Select Corresponding Point')
+                self.tracker = self.tracker + 1
+
+            elif self.tracker == 4:
+                # coordinates from 2nd corresponding spot on live
+                coordinates = self.imagepaneref.imagepane._fixLoc
+                self.ptli2 = numpy.float32(coordinates)
+
+                # change button label for next point
+                self.Cali.SetLabel('Select 3rd Point on Image')
+                self.tracker = self.tracker + 1
+
+            elif self.tracker == 5:
+                # coordinates from 3rd spot on image
+                coordinates = self.imagepaneref.imagepane._fixLoc
+                self.ptim3 = numpy.float32(coordinates)
+
+                # change button label for next point
+                self.Cali.SetLabel('Select Corresponding Point')
+                self.tracker = self.tracker + 1
+
+            elif self.tracker == 6:
+                # coordinates from 3rd corresponding spot on live
+                coordinates = self.imagepaneref.imagepane._fixLoc
+                self.ptli3 = numpy.float32(coordinates)
+
+                # change button label for next point
+                self.Cali.SetLabel('Calibrate')
+                self.tracker = self.tracker + 1
+
+            elif self.tracker == 7:
+                # Putting all the coordinates together
+                self.ptsim = numpy.float32([self.ptim1, self.ptim2, self.ptim3])
+                self.ptsli = numpy.float32([self.ptli1, self.ptli2, self.ptli3])
+
+                # The Affine Transformation
+                self.matrix = cv2.getAffineTransform(self.ptsim, self.ptsli)
+                self.result = cv2.warpAffine(self.img, self.matrix, (self.cols, self.rows))
+
+                # https://www.geeksforgeeks.org/python-opencv-cv2-imwrite-method/
+                # save the transformed image temporarily
+                filename = 'tempim.TIF'
+                os.chdir(self.header_dir)
+                cv2.imwrite(filename, self.result)
+                impath = self.header_dir + os.sep + filename
+
+                # https://wxpython.org/Phoenix/docs/html/wx.Image.html#wx.Image.LoadFile
+                # Load in the file we just saved so we can make it a bitmap
+                self.bkgrdim = wx.Bitmap(1, 1)
+                self.bkgrdim.LoadFile(impath, wx.BITMAP_TYPE_ANY)
+
+                # reset the background to the new transformed image
+                self.imagepaneref.call_set_bkrgd(self.bkgrdim)
+
+                # change button label
+                self.Cali.SetLabel('New Calibration')
+                self.tracker = 0
+
+                # delete the temporary file we made
+                os.remove(filename)
+
+        # if pressed == self.zoom:
+        #     print("Hello")
+        #     wx.Bitmap.SetSize(self.bkgrdim, (512, 512))
+        # if pressed == self.pan:
+        #     print("How are you?")
+        # if pressed == self.rotate:
+        #     print("Bomb diggity")
+
+    def on_open_background_image(self, evt=None):
+        dialog = wx.FileDialog(self, 'Select background image:', self.header_dir, self.filename,
+                               'Image files (*.jpg,*.jpeg,*.bmp,*.png,*.tif,*.tiff)| *.jpg;*.jpeg;*.bmp;*.png;*.tif;*.tiff|' +
+                               'JP(E)G images (*.jpg,*.jpeg)|*.jpg;*.jpeg|BMP images (*.bmp)|*.bmp' +
+                               '|PNG images (*.png)|*.png|TIF(F) images (*.tif,*.tiff)|*.tif;*.tiff', wx.FD_OPEN)
+        if dialog.ShowModal() == wx.ID_OK:
+            self.header_dir = dialog.GetDirectory()
+            self.filename = dialog.GetFilename()
+            dialog.Destroy()
+            impath = self.header_dir + os.sep + self.filename
+
+            self.bkgrdim = wx.Bitmap(1, 1)
+            self.bkgrdim.LoadFile(impath, wx.BITMAP_TYPE_ANY)
+            # JG affine transformation attempt
+            self.img = cv2.imread(impath)
+            self.rows, self.cols, self.ch = self.img.shape
+
+            self.imagepaneref.call_set_bkrgd(self.bkgrdim)
+            #self.imagepaneref.set_bkgrd(self.bkgrdim)
+
+            # dlg = wx.TextEntryDialog(self, 'Image Scale X Dimension:', 'Image Scale')
+            # if dlg.ShowModal() == wx.ID_OK:
+            #     self.xdim = dlg.GetValue()
+            # dlg.Destroy()
+            # print('x dimension is: ', int(self.xdim))
+            #
+            # dlg = wx.TextEntryDialog(self, 'Image Scale Y Dimension:', 'Image Scale')
+            # if dlg.ShowModal() == wx.ID_OK:
+            #     self.ydim = dlg.GetValue()
+            # dlg.Destroy()
+            # print('y dimension is: ', int(self.ydim))
+            #
+            # # make a string out of the resolution dimensions
+            # resolution = str(self.xdim) + ' x ' + str(self.ydim)
+            # # reset the res label from No Image to the resolution that was entered
+            # print(hex(id(self.resref)))
+            # self.sidepane.res.SetLabel(resolution)
 
 
 
